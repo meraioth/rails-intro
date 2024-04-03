17b616ed03beb99df82bff95b34e8755a72f6241
Meraioth Ulloa Salazar <mulloa@buk.cl>
2024-04-03 00:28:40 -0300

    feat: crea modelo comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_025739) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_032813) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "reaction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.string "title"
