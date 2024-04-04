c5fdea5be7a707dccba488ae3365a8b119b678f8
Meraioth Ulloa Salazar <mulloa@buk.cl>
2024-04-03 23:46:07 -0300

    feat: agrega anotaciones a modelos

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  reaction   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :integer          not null
+#
+# Foreign Keys
+#
+#  post_id  (post_id => posts.id)
+#
 class Comment < ApplicationRecord
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  title      :string
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
   has_many :comments
 end
