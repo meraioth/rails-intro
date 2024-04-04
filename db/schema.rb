79831bcb69aeaf9c52072f8ab20dbc2c01574c87
Meraioth Ulloa Salazar <mulloa@buk.cl>
2024-04-04 20:42:04 -0300

    feat: agrega fk de user en posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_04_013717) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_04_204100) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "reaction"
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_04_013717) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_04_013717) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
