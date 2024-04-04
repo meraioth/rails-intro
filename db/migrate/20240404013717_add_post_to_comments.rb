9f46938bdbd9327ded94639449156b2789a89abd
Meraioth Ulloa Salazar <mulloa@buk.cl>
2024-04-03 22:40:43 -0300

    feat: crea relacion post y comments

---
+++ db/migrate/20240404013717_add_post_to_comments.rb
@@ -0,0 +1,5 @@
+class AddPostToComments < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :comments, :post, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_032813) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_04_013717) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "reaction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_032813) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
