5c0cbe3b35f276cdc7d698b29cfb8f44704f8f78
Meraioth Ulloa Salazar <mulloa@buk.cl>
2024-04-04 21:40:42 -0300

    feat: agrega fk user a comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_04_204100) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_04_235100) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "reaction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_id", null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -43,5 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_04_204100) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
